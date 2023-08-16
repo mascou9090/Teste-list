b1222117b9d7821fcd96b30f6beed7cb6f88e7f4
Marcondes Ferreira <bshookss@gmail.com>
2023-08-16 19:32:31 -0300

    Add project files.

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+            
+        }
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public void IncreaseSalary(double percent) { 
+            Salary += Salary / percent;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + " Name: " + Name + " Salary: " + Salary;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.WriteLine("How many employees will be registered?");
+            int employeeAux = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < employeeAux; i++)
+            {
+                Console.WriteLine($"Employee #{i + 1}");
+                Console.Write("Numero do id:");
+                int idAux = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string nameAux = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salaryAux = double.Parse(Console.ReadLine());
+
+                employees.Add(new Employee(idAux, nameAux, salaryAux));
+            };
+
+            Console.WriteLine("Enter the employee id that will have salary increase :");
+            int idEmployeeSearch = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the percentage: ");
+            double percentAux = double.Parse(Console.ReadLine());
+
+
+            int indexEmployee = employees.FindIndex(x => x.Id == idEmployeeSearch);
+
+            if(indexEmployee != -1)
+            {
+                Employee employeChange = employees.Find(x => x.Id == idEmployeeSearch);
+
+                int indexEmployeeRight = employees.FindIndex(x => x.Id == idEmployeeSearch);
+
+                employeChange.IncreaseSalary(percentAux);
+
+                employees.Insert(indexEmployeeRight, employeChange);
+            } else
+            {
+                Console.WriteLine("This id doesn't exist!");
+            }
+
+
+            Console.WriteLine("Updated list of employees:");
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
